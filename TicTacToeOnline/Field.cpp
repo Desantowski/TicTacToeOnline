3539c00fa421796e489106da2c9ca9a02908dd87
Patrick D <desantowski@gmail.com>
2017-08-29 16:44:17 +0200

    Field c-tor patch

--- TicTacToeOnline/Field.cpp
+++ TicTacToeOnline/Field.cpp
@@ -2,6 +2,7 @@
 
 Field::Field(sf::FloatRect position, sf::Texture & texture) : sf::Sprite{ texture }
 {
+	setPosition(position.left, position.top);
 	setScale(sf::Vector2f{ position.height / texture.getSize().x,position.width / texture.getSize().y });
 }
 
