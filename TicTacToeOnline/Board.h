636c17bc8430fe18e5065ff6fda443913a5774d0
Patrick D <desantowski@gmail.com>
2017-08-29 13:39:37 +0200

    Declaration for Board::makeMove has been changed

--- TicTacToeOnline/Board.h
+++ TicTacToeOnline/Board.h
@@ -8,8 +8,7 @@ class Board
 	std::array<Field, 9> fields;
 public:
 	void draw(sf::RenderTarget & target);
-	void makeMove(Player::Type playerType, unsigned char fieldID);
-	unsigned char getFieldIDByPosition(sf::Vector2f position);
+	void makeMove(Player::Type playerType, sf::Vector2f position);
 	Board();
 	~Board();
 };
