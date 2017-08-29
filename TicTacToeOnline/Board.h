56014a3300cda5f62da8abedec20694191e46cb6
Patrick D <desantowski@gmail.com>
2017-08-29 15:33:12 +0200

    Declaration for static void Board::prepareBoard()

--- TicTacToeOnline/Board.h
+++ TicTacToeOnline/Board.h
@@ -7,17 +7,21 @@
 
 class Board
 {
-	Field& getFieldByPosition(sf::Vector2f position);
-	sf::Texture& getTextureByType(Player::Type playerType);
-
 	static const std::unique_ptr<sf::Texture> mFieldXPtr;
 	static const std::unique_ptr<sf::Texture> mFieldOPtr;
 	static const std::unique_ptr<sf::Texture> mFieldNullPtr;
+	static constexpr float mFieldHeight{ 100 }, mFieldWidth{ 100 };
+
+	Field& getFieldByPosition(sf::Vector2f position);
+	sf::Texture& getTextureByType(Player::Type playerType);
 
 	std::vector<Field> mFields;
 	sf::Vector2f mLeftTop{ 300,400 };
 
 public:
+	static void prepareBoard();
+
+	
 	void draw(sf::RenderTarget & target);
 	void makeMove(Player::Type playerType, sf::Vector2f position);
 	Board(sf::Vector2f windowSize);
