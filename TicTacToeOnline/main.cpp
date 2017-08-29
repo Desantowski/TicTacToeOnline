dcac679af2a26971e8fcb3988b3661e5fc3284e4
Patrick D <desantowski@gmail.com>
2017-08-29 13:25:52 +0200

    Deleted unnecesary files

--- TicTacToeOnline/main.cpp
+++ TicTacToeOnline/main.cpp
@@ -1,8 +1,5 @@
-#include <SFML\Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-	sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode(800, 600), "Test");
-	while (window->isOpen()) { std::cout << "Y\n"; }
 }
