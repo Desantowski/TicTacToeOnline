2abbb2390ce4264a454e4807730c1e4e9f8c3db2
Patrick D <desantowski@gmail.com>
2017-08-30 19:09:11 +0200

    Moved definition of PlayingState to header file

--- TicTacToeOnline/PlayingState.h
+++ TicTacToeOnline/PlayingState.h
@@ -15,9 +15,36 @@ class PlayingState : public GameState
 	std::shared_ptr<Player> m_currentPlayer;
 
 public:
-	virtual void processEvents(sf::RenderWindow & source) override;
-	virtual void processGraphic(sf::RenderTarget & target) override;
-	virtual void tick() override;
-	PlayingState(sf::Window & window, EnemyType enemy);
-	~PlayingState();
+
+	virtual void processEvents(sf::RenderWindow & source) override
+	{
+		while (source.pollEvent(m_event))
+		{
+			if (m_event.type == sf::Event::Closed)
+			{
+				source.close();
+			}
+		}
+	}
+
+	virtual void processGraphic(sf::RenderTarget & target) override
+	{
+		m_board->draw(target);
+	}
+
+	virtual void tick() override
+	{
+		return;
+	}
+
+
+	PlayingState(sf::Window & window, EnemyType enemy) : m_board{ new Board{ window.getSize() } },
+		m_player{ new Player{ Player::Type::PLAYER_O } },
+		m_enemy{ new EnemyType { enemy } }
+	{
+	}
+
+	~PlayingState()
+	{
+	}
 };
