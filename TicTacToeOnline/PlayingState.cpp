2d6d1841e419702d6ed25b3c8997c8b514eae2ce
Patrick D <desantowski@gmail.com>
2017-09-01 00:14:41 +0200

    Basic bot implementation and game is (very basic) playable now

--- TicTacToeOnline/PlayingState.cpp
+++ TicTacToeOnline/PlayingState.cpp
@@ -1,1 +1,35 @@
 #include "PlayingState.h"
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+template<class T>
+void PlayingState<T>::enemyMove()
+{
+	throw UnknownEnemyTypeException();
+}
+
+template<>
+void PlayingState<BotPlayer>::enemyMove()
+{
+	static std::default_random_engine generator;
+	static std::uniform_real_distribution<float> distribution(0, 800);
+
+	float f1 = distribution(generator), f2 = distribution(generator);
+
+	if (m_board->makeMove(m_enemy->getType(), sf::Vector2f{ f1, f2 }))
+		m_currentPlayer = m_player;
+}
+
+template<>
+void PlayingState<Player>::enemyMove()
+{
+
+	static std::default_random_engine generator;
+	static std::uniform_real_distribution<float> distribution(0, 800);
+
+	float f1 = distribution(generator), f2 = distribution(generator);
+
+	if (m_board->makeMove(m_enemy->getType(), sf::Vector2f{ f1, f2 } ))
+		m_currentPlayer = m_player;
+}
--- TicTacToeOnline/PlayingState.h
+++ TicTacToeOnline/PlayingState.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <memory>
+#include <random>
 #include "GameState.h"
 #include "Player.h"
+#include "BotPlayer.h"
 #include "Board.h"
 
 template<class EnemyType>
@@ -11,11 +13,21 @@ class PlayingState : public GameState
 
 	std::shared_ptr<Player> m_player;
 	std::shared_ptr<EnemyType> m_enemy;
-
 	std::shared_ptr<Player> m_currentPlayer;
+	std::shared_ptr<Player> m_winner;
+
+	std::unique_ptr<sf::Event> m_playerClick;
+
+	bool playerMove()
+	{
+		auto result = m_board->makeMove(m_currentPlayer->getType(), sf::Vector2f{ static_cast<float>(m_playerClick->mouseButton.x), static_cast<float>(m_playerClick->mouseButton.y) });
+		m_playerClick = nullptr;
+		return result;
+	}
+
+	void enemyMove();
 
 public:
-
 	virtual void processEvents(sf::RenderWindow & source) override
 	{
 		while (source.pollEvent(m_event))
@@ -23,6 +35,10 @@ public:
 			if (m_event.type == sf::Event::Closed)
 			{
 				source.close();
+			}
+			else if (m_event.type == sf::Event::MouseButtonPressed && m_event.mouseButton.button == sf::Mouse::Button::Left)
+			{
+				m_playerClick = std::make_unique<sf::Event>( m_event );
 			}
 		}
 	}
@@ -32,15 +48,25 @@ public:
 		m_board->draw(target);
 	}
 
-	virtual void tick() override
+	virtual void tick(sf::RenderWindow & window) override
 	{
+		if (m_winner != nullptr) return;
+		if (m_currentPlayer == m_player)
+		{
+			if (m_playerClick != nullptr)
+			{
+				if (playerMove()) m_currentPlayer = m_enemy;
+			}
+		}
+		else enemyMove();
+		if (m_board->getWinnerType() != Field::Type::FIELD_NULL) m_winner = std::make_shared<Player>(*m_currentPlayer);
 		return;
 	}
 
-
 	PlayingState(sf::Window & window, EnemyType enemy) : m_board{ new Board{ window.getSize() } },
 		m_player{ new Player{ Player::Type::PLAYER_O } },
-		m_enemy{ new EnemyType { enemy } }
+		m_enemy{ new EnemyType { enemy } },
+		m_currentPlayer { m_player }
 	{
 	}
 
